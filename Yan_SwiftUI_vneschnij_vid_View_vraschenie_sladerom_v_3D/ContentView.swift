22a44f051320826edda1b9aaa902c7648632fc85
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 06:18:04 +0500

    Initial Commit

---
+++ Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3D/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3D
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3D/Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3DApp.swift
+//  Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3D
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_vneschnij_vid_View_vraschenie_sladerom_v_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
